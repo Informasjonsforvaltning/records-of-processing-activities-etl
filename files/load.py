4fae704dfbe457e31e2ce4c97164cabfb8578240
Hans Olav Sund <hans.olav.sund@digdir.no>
2021-10-06 14:55:53 +0200

    chore: extract finished (?)

--- files/load.py
+++ files/load.py
@@ -2,7 +2,7 @@ import json
 import os
 from pymongo import MongoClient
 import argparse
-
+from bson.json_util import loads
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--outputdirectory', help="the path to the directory of the output files", required=True)
@@ -11,8 +11,9 @@ connection = MongoClient(
     f"""mongodb://{os.environ['MONGO_USERNAME']}:{os.environ['MONGO_PASSWORD']}@mongodb:27017/recordsDB?authSource=admin&authMechanism=SCRAM-SHA-1""")
 db = connection.recordsDB
 
-with open(args.outputdirectory + 'transformed_datasets.json') as catalogs_file:
-    transformed_json = json.load(catalogs_file)
+with open(args.outputdirectory + 'transformed_records.json') as records_file:
+    records_file_str = records_file.read()
+    transformed_json = loads(records_file_str)
 
     total_updated = 0
     total_failed = 0
@@ -20,16 +21,40 @@ with open(args.outputdirectory + 'transformed_datasets.json') as catalogs_file:
     for mongo_id in transformed_json:
         to_be_updated = transformed_json[mongo_id]
         print("Updating ID: " + mongo_id)
-        insert_result = db.datasets.find_one_and_update({'_id': mongo_id},  {'$set': to_be_updated})
+        insert_result = db.records.find_one_and_update({'_id': mongo_id},  {'$set': to_be_updated}, {'upsert': 'true'})
         if insert_result:
             total_updated += 1
             print("Successfully updated: " + mongo_id)
         else:
             total_failed += 1
             print("Update failed: " + mongo_id)
-            fail_log[mongo_id] = transformed_json[mongo_id]
+            fail_log[mongo_id] = mongo_id
         total_updated += 1
-    print("Total number of datasets updated: " + str(total_updated))
-    print("Total number of datasets updates failed: " + str(total_failed))
+    print("Total number of records updated: " + str(total_updated))
+    print("Total number of record updates failed: " + str(total_failed))
+    with open("load_errors.json", 'w', encoding="utf-8") as err_file:
+        json.dump(fail_log, err_file, ensure_ascii=False, indent=4)
+
+with open(args.outputdirectory + 'transformed_representatives.json') as representatives_file:
+    representatives_file_str = representatives_file.read()
+    transformed_json = loads(representatives_file_str)
+
+    total_updated = 0
+    total_failed = 0
+    fail_log = {}
+    for mongo_id in transformed_json:
+        to_be_updated = transformed_json[mongo_id]
+        print("Updating ID: " + mongo_id)
+        insert_result = db.representatives.find_one_and_update({'_id': mongo_id},  {'$set': to_be_updated}, {'upsert': 'true'})
+        if insert_result:
+            total_updated += 1
+            print("Successfully updated: " + mongo_id)
+        else:
+            total_failed += 1
+            print("Update failed: " + mongo_id)
+            fail_log[mongo_id] = mongo_id
+        total_updated += 1
+    print("Total number of representatives updated: " + str(total_updated))
+    print("Total number of representative updates failed: " + str(total_failed))
     with open("load_errors.json", 'w', encoding="utf-8") as err_file:
         json.dump(fail_log, err_file, ensure_ascii=False, indent=4)
